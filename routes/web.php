79464840c0b40b11873fc666fd8f88e4898dbea9
Nour Ashraf <nono3nom235gmail.com>
2022-06-24 19:22:02 +0200

    sim routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/servvices', function () {
+    return view('servvices');
 });
