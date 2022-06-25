1f42a3bba42d70507081b202d212bd293d86ad62
Nour Ashraf <nono3nom235gmail.com>
2022-06-25 19:03:44 +0200

    prouduct model

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -15,9 +15,10 @@
             <a class="navbar-brand" href="#">Laravel 7.X</a>
             </div>
             <ul class="nav navbar-nav">
-                <li><a href="/">Home</a></li>
+                <li><a href="/" =>Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/services/nour/2"   >Services</a></li>
+                <li><a href={{ URL::to("/services/nour/") }}   >Services</a></li>
             </ul>
              <!--<ul class="nav navbar-nav navbar-right">
                 <li><a href="#">New product</a></li>
